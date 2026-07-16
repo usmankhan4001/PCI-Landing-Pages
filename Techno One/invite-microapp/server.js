a7d5ef3725fb7ec4a3dfe856d0c46b1e07d73d19
usmankhan4001 <ranausmankhan4001@gmail.com>
2026-07-16 19:11:04 +0500

    Add Techno One invitation microapp

---
+++ Techno One/invite-microapp/server.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+app.disable('x-powered-by');
+
+app.use((_req, res, next) => {
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+  next();
+});
+
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: '1d' }));
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const port = process.env.PORT || 3100;
+app.listen(port, () => {
+  console.log(`Techno One invite microapp listening on :${port}`);
+});
